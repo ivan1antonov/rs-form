99d2f2c1efe85861f29d7c9c1a4e632d47e9f426
Ivan Antonov <ivan1antonov@gmail.com>
2025-08-24 15:30:11 +0400

    feat: add prettier, husky, redux, reducer form, store, add store to provider for main.tsx, create MainPage, add type form

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { store } from './store';
+import { Provider } from 'react-redux';
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById('root');
+
+if (!root) {
+  throw new Error('Sorry, root does not exist');
+}
+
+createRoot(root).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
